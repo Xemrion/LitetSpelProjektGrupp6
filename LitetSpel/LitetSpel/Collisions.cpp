6878303be54fd274f1d20e7185a8dc5c41a7d4ea
Falkgaard <falkgaard@gmail.com>
2019-04-22 21:39:12 +0200

    Test

--- LitetSpel/LitetSpel/Collisions.cpp
+++ LitetSpel/LitetSpel/Collisions.cpp
@@ -9,10 +9,10 @@ bool IObject::operator!=( IObject const &other ) const noexcept {
 
 
 CollisionManager::CollisionManager() {
-    _staticBoxes.reserve(  200 ); // arbitrary constants to minimize vector copying
-    _mobileBoxes.reserve(   25 ); // arbitrary constants to minimize vector copying
-    _staticSpheres.reserve( 10 ); // arbitrary constants to minimize vector copying
-    _mobileSpheres.reserve(  5 ); // arbitrary constants to minimize vector copying
+    _staticBoxes.reserve(  499 ); // arbitrary constants to minimize vector copying
+    _mobileBoxes.reserve(   49 ); // arbitrary constants to minimize vector copying
+    _staticSpheres.reserve( 99 ); // arbitrary constants to minimize vector copying
+    _mobileSpheres.reserve( 49 ); // arbitrary constants to minimize vector copying
 }
 
 // possibility: separate base classes for mobiles and statics and use function specialization instead of the final bool
@@ -33,69 +33,48 @@ void CollisionManager::register_entry( IObject &parent, CollisionId id, Sphere c
 
 // TODO: wrap with RAII
 void CollisionManager::unregister_entry( IObject const &parent ) noexcept {
-    auto unary_predicate = [&parent]( auto const &e ) { return *(e.object) != parent; };
-    std::remove_if( _staticBoxes.begin(),   _staticBoxes.end(),   unary_predicate );
-    std::remove_if( _mobileBoxes.begin(),   _mobileBoxes.end(),   unary_predicate );
+    auto unary_predicate = [&parent]( auto const &e ) { return !(*(e.object) != parent); };
+    std::remove_if(   _staticBoxes.begin(),   _staticBoxes.end(), unary_predicate );
+    std::remove_if(   _mobileBoxes.begin(),   _mobileBoxes.end(), unary_predicate );
     std::remove_if( _staticSpheres.begin(), _staticSpheres.end(), unary_predicate );
     std::remove_if( _mobileSpheres.begin(), _mobileSpheres.end(), unary_predicate );
 }
 
-[[nodiscard]] bool CollisionManager::intersect( Box const &a, Box const &b ) noexcept {
-    auto constexpr x { 0U };
-    auto constexpr y { 1U };
-    auto const dt_x    = abs(a.center[x]   - b.center[x])      * 2; // midpoint delta x
-    auto const dt_y    = abs(a.center[y]   - b.center[y])      * 2; // midpoint delta y
-    auto const cWidth  = (a.halfLengths[x] + b.halfLengths[x]) * 2; // combined width
-    auto const cHeight = (a.halfLengths[y] + b.halfLengths[y]) * 2; // combined height
-    auto result =  dt_x < cWidth  and  dt_y < cHeight;
-    return result;
+[[nodiscard]] bool CollisionManager::intersect( Box const *a, Box const *b ) noexcept {
+    auto const dt_x    = abs(a->center.x   - b->center.x)      * 2; // midpoint delta x
+    auto const dt_y    = abs(a->center.y   - b->center.y)      * 2; // midpoint delta y
+    auto const cWidth  = (a->halfLengths.x + b->halfLengths.x) * 2; // combined width
+    auto const cHeight = (a->halfLengths.y + b->halfLengths.y) * 2; // combined height
+    return dt_x < cWidth  and  dt_y < cHeight;
 }
 
-[[nodiscard]] bool CollisionManager::intersect( Sphere const &a, Box const &b ) noexcept {
+[[nodiscard]] bool CollisionManager::intersect( Sphere const *a, Box const *b ) noexcept {
     // sphere center
-    auto const    &s = a.centerRadius;
-
+    auto const &s = a->centerRadius;
     // box center
-    auto bc = glm::vec2( b.center.x, b.center.y );
-
-    auto constexpr sqr = []( auto n ) { return n*n; }; // TODO: use std or ppm
+    auto bc = glm::vec2( b->center.x, b->center.y );
     // distance between box and sphere centers
     auto dtX = abs(s.x - bc.x);
     auto dtY = abs(s.y - bc.y);
-
-
     // check if they're too far apart to intersect
-    if ( dtX > (b.halfLengths.x + s.w) )
+    if ( dtX > (b->halfLengths.x + s.w)  or  dtY > (b->halfLengths.y + s.w) )
         return false;
-    if ( dtY > (b.halfLengths.y + s.w) )
-        return false;
-
     // check if they're too close not to intersect
-    if ( dtX <= b.halfLengths.x )
+    if ( dtX <= b->halfLengths.x  or  dtY <= b->halfLengths.y )
         return true;
-    if ( dtY <= b.halfLengths.y )
-        return true;
-
-
-    // else handle the corner cases:
-    auto sqrVertDist = sqr(dtX - b.halfLengths.x) + sqr(dtY - b.halfLengths.y);
-    bool tmp;
-    return tmp = sqr(s.w) > sqrVertDist;
+    // handle the corner cases:
+    auto sqrVertDist = sqr(dtX - b->halfLengths.x) + sqr(dtY - b->halfLengths.y);
+    return sqr(s.w) > sqrVertDist;
 }
 
-[[nodiscard]] inline bool CollisionManager::intersect( Box const &a, Sphere const &b ) noexcept {
+[[nodiscard]] inline bool CollisionManager::intersect( Box const *a, Sphere const *b ) noexcept {
     return intersect(b,a);
 }
 
-[[nodiscard]] bool CollisionManager::intersect( Sphere const &s1, Sphere const &s2 ) noexcept {
-    auto const    &a = s1.centerRadius;
-    auto const    &b = s2.centerRadius;
-    auto constexpr x { 0U };
-    auto constexpr y { 1U };
-    auto constexpr r { 3U };
-    auto constexpr sqr = []( auto n ) { return n*n; }; // TODO: use std or ppm
-    auto result = sqr(a[x]-b[x]) + sqr(a[y]-b[y])  <  sqr(a[r]+b[r]);
-    return result;
+[[nodiscard]] bool CollisionManager::intersect( Sphere const *s1, Sphere const *s2 ) noexcept {
+    auto const &a = s1->centerRadius;
+    auto const &b = s2->centerRadius;
+    return sqr(a.x-b.x) + sqr(a.y-b.y)  <  sqr(a.w + b.w);
 }
  
 // possibilities: cull distant objects based off of a preliminary spatial eligibility test
@@ -103,40 +82,58 @@ void CollisionManager::unregister_entry( IObject const &parent ) noexcept {
 void CollisionManager::update() { /// yay for combinatoric explosions /s
     // test each mobile hitbox for intersections:
     for ( auto &m : _mobileBoxes ) {
+        auto &mObject = *(m.object);
         // test against all other mobile hitboxes (that belong to another object)
-        for ( auto &other : _mobileBoxes )
-            if ( *(m.object) != *(other.object)  and  intersect( *(m.hitbox), *(other.hitbox) ) )
-                   m.object->collide( m.id, other.id, *(other.object) );
+        for ( auto &other : _mobileBoxes ) {
+            auto &otherObject = *(other.object);
+            if ( mObject != otherObject  and  intersect( m.hitbox, other.hitbox ) )
+                   m.object->collide( m.id, other.id, otherObject );
+        }
         // test against all mobile hitspheres (that belong to another object)
-        for ( auto &other : _mobileSpheres )
-            if ( *(m.object) != *(other.object)  and  intersect( *(m.hitbox), *(other.hitsphere) ) )
-                m.object->collide( m.id, other.id, *(other.object) );
+        for ( auto &other : _mobileSpheres ) {
+            auto &otherObject = *(other.object);
+            if ( mObject != otherObject  and  intersect( m.hitbox, other.hitsphere ) )
+                m.object->collide( m.id, other.id, otherObject );
+        }
         // test against all static environment hitboxes
-        for ( auto &environment : _staticBoxes )
-            if ( intersect( *(m.hitbox), *(environment.hitbox) ) )
-                m.object->collide( m.id, environment.id, *(environment.object) );
+        for ( auto &environment : _staticBoxes ) {
+            auto &environmentObject = *(environment.object);
+            if ( intersect( m.hitbox, environment.hitbox ) )
+                m.object->collide( m.id, environment.id, environmentObject );
+        }
         // test against all static environment hitspheres
-        for ( auto &environment : _staticSpheres )
-            if ( intersect( *(m.hitbox), *(environment.hitsphere) ) )
-                m.object->collide( m.id, environment.id, *(environment.object) );
+        for ( auto &environment : _staticSpheres ) {
+            auto &environmentObject = *(environment.object);
+            if ( intersect( m.hitbox, environment.hitsphere ) )
+                m.object->collide( m.id, environment.id, environmentObject );
+        }
     }
     // now repeat the process for all the mobiles hitspheres:
     for ( auto &m : _mobileSpheres ) {
+        auto &mObject = *(m.object);
         // test against all mobile hitspheres (that belong to another object)
-        for ( auto &other : _mobileSpheres )
-            if ( *(m.object) != *(other.object)  and  intersect( *(m.hitsphere), *(other.hitsphere) ) )
-                m.object->collide( m.id, other.id, *(other.object) );
+        for ( auto &other : _mobileSpheres ) {
+            auto &otherObject = *(other.object);
+            if ( mObject != otherObject  and  intersect( m.hitsphere, other.hitsphere ) )
+                mObject.collide( m.id, other.id, otherObject );
+        }
         // test against all mobile hitboxes (that belong to another object)
-        for ( auto &other : _mobileBoxes )
-            if ( *(m.object) != *(other.object)  and  intersect( *(m.hitsphere), *(other.hitbox) ) )
-                m.object->collide( m.id, other.id, *(other.object) );
+        for ( auto &other : _mobileBoxes ) {
+            auto &otherObject = *(other.object);
+            if ( *(m.object) != otherObject  and  intersect( m.hitsphere, other.hitbox ) )
+                mObject.collide( m.id, other.id, otherObject );
+        }
         // test against all static environment hitspheres
-        for ( auto &environment : _staticSpheres )
-            if ( intersect( *(m.hitsphere), *(environment.hitsphere) ) )
-                m.object->collide( m.id, environment.id, *(environment.object) );
+        for ( auto &environment : _staticSpheres ) {
+            auto &environmentObject = *(environment.object);
+            if ( intersect(m.hitsphere, environment.hitsphere ) )
+                mObject.collide( m.id, environment.id, *(environment.object) );
+        }
         // test against all static environment hitboxes
-        for ( auto &environment : _staticBoxes )
-            if ( intersect( *(m.hitsphere), *(environment.hitbox) ) )
-                m.object->collide( m.id, environment.id, *(environment.object) );
+        for ( auto &environment : _staticBoxes ) {
+            auto &environmentObject = *(environment.object);
+            if ( intersect( m.hitsphere, environment.hitbox ) )
+                mObject.collide( m.id, environment.id, *(environment.object) );
+        }
     }
 }
--- LitetSpel/LitetSpel/Collisions.h
+++ LitetSpel/LitetSpel/Collisions.h
@@ -39,16 +39,166 @@ constexpr bool debugCollisions = true;
 //     }
 // }
 
+// lasrar
+// spikfällor
+// pressar
+// kraftfält
+// knuffblock
+// spakar
+// knappar
+
+
+// s = playerSphere.centerRadius
+
+     // mid.X               mid.Y                    halfX       halfY
+//*A*/   x= s.x              y=s.y + 0.8*s.w          1.6*s.w     0.2*s.w
+//*B*/   x= s.x - 0.8*s.w    y=s.y                    0.2*s.w     2.0*s.w
+//*C*/   x= s.x + 0.8*s.w    y=s.y                    0.2*s.w     2.0*s.w
+//*D*/   x= s.x              y=s.y - 0.2*s.w          1.6*s.w     1.8*s.w
+
+
+// class ISlider
+// {
+// public:
+//     ISlider() {}
+//     virtual ~ISlider() {}
+// 
+//     void update(double dt) noexcept {
+//         
+//         if ( _target.position  != _path[_i] )
+//             ; // TODO: dt * move _speed along vector between _target.pos och *_current_target
+//         else { // target reached
+//             if ( _isMovingForward ) {
+//                  ++_i;
+//                  if ( _i == _path.size()-1 )
+//                      _isMovingForward = false;
+//             }
+//             else {
+//                 --_i;
+//                 if ( _i == 0 )
+//                     _isMovingForward = true;
+//             }
+//         }
+//     }
+// 
+// private:
+//     bool                    _isMovingForward;
+//     size_t                  _i;
+//     float                   _speed;
+//     IObject                &_target;
+//     std::vector<glm::vec2>  _path;
+// };
+// 
+// class IActivable {
+// public:
+//     IActivable( bool isActive=false ):
+//         _isActive(false)
+//     {
+//         if (isActive) toggle();
+//     }
+// 
+//     virtual ~IActivable() {}
+// 
+//     virtual bool onActivation()   noexcept {}
+//     virtual bool onDeactivation() noexcept {}
+// 
+//     void toggle() noexcept {
+//         _isActive = !_isActive;
+//         if ( _isActive )
+//             onActivation();
+//         else onDeactivation();
+//     }
+// 
+//     bool isActive() const { return _isActive; }
+// 
+// private:
+//     bool _isActive;
+// };
+// 
+// class PeriodicActivator : public IActivable {
+// public:
+//     PeriodicActivator(IActivable &target, double uptime, double downtime, bool startsActive):
+//         IActivable (startsActive),
+//         _target    (target),
+//         _downtime  (downtime),
+//         _uptime    (uptime),
+//         _timeLeft  (startsActive? uptime : downtime)
+//     {}
+// 
+//     virtual ~PeriodicActivator() {}
+// 
+//     virtual bool onActivation()   noexcept { _target.toggle(); } // forward signal
+//     virtual bool onDeactivation() noexcept { _target.toggle(); } // forward signal
+// 
+//     virtual void update( double dt ) {
+//         _timeLeft -= dt;
+//         if ( _timeLeft < 0 ) {
+//             toggle();
+//             if ( isActive() )
+//                  _timeLeft = _uptime;
+//             else _timeLeft = _downtime;
+//         }
+//     }
+// 
+// private:
+//     IActivable &_target;
+//     double _downtime, _uptime, _timeLeft;
+// };
+// 
+// class Lever : public IObject, public IActivable {
+//     Lever(IActivable &target, bool isActive=false):
+//         IObject(),
+//         IActivable(isActive),
+//         _target(target)
+//     {}
+// 
+//     virtual ~Lever() {}
+// 
+//     virtual bool onActivation()   noexcept { _target.toggle(); } // forward signal
+//     virtual bool onDeactivation() noexcept { _target.toggle(); } // forward signal
+// 
+//     // TODO: collide() som kollar PlayerInteraction och kör toggle()
+//     // TODO: rotationslogik
+// 
+// private:
+//     IActivable &_target;
+//     Sphere _hitbox; // vid kollision, toggle() och rotera _stick
+//     Box    _base;
+//     Box    _stick;
+// };
+// 
+// class Button : public IObject, public IActivable {
+//     Button(IActivable &target, bool isActive=false):
+//         IObject(),
+//         IActivable(isActive),
+//         _target(target)
+//     {}
+// 
+//     virtual ~Button() {}
+// 
+//     virtual bool onActivation()   noexcept { _target.toggle(); } // forward signal
+//     virtual bool onDeactivation() noexcept { _target.toggle(); } // forward signal
+// 
+//     // TODO: collide() som kollar tung player eller tungt block mot 
+// 
+// private:
+//     IActivable &_target;
+//     Sphere      _hitboxButton; // vid kollision, toggle() och höj/sänk _button
+//     Box         _base;
+//     Box         _button;
+// };
 
 
 // TODO: contemplate placement;
 enum CollisionId { player_top,
                    player_bottom,
-                   player_side,
+                   player_left,
+                   player_right,
                    platform,
                    wall,
                    powerup_bouncy,
-                   powerup_sticky
+                   powerup_sticky,
+                   powerup_heavy
                    /* TODO: populate */ };
 
 // TODO: refactor into more suitable header/source?
@@ -64,6 +214,8 @@ public:
     virtual ~IObject() noexcept {}
 
     virtual void collide( CollisionId ownHitbox, CollisionId otherHitbox, IObject &other ) = 0;
+
+    virtual void update(double dt) noexcept {}
 
     [[nodiscard]] auto inline getId() const noexcept { return _id; }
 
@@ -88,10 +240,10 @@ public:
     void unregister_entry( IObject const &parent ) noexcept;
 
     // evaluates whether two hitboxes are intersecting or not
-    [[nodiscard]]        bool intersect(    Box const &a,    Box const &b ) noexcept;
-    [[nodiscard]]        bool intersect( Sphere const &a,    Box const &b ) noexcept;
-    [[nodiscard]] inline bool intersect(    Box const &a, Sphere const &b ) noexcept;
-    [[nodiscard]]        bool intersect( Sphere const &a, Sphere const &b ) noexcept;
+    [[nodiscard]]        bool intersect(    Box const *a,    Box const *b ) noexcept;
+    [[nodiscard]]        bool intersect( Sphere const *a,    Box const *b ) noexcept;
+    [[nodiscard]] inline bool intersect(    Box const *a, Sphere const *b ) noexcept;
+    [[nodiscard]]        bool intersect( Sphere const *a, Sphere const *b ) noexcept;
 
     // checks if any of the mobiles have collided,
     // and if so, notifies them with the relevant information
@@ -99,6 +251,8 @@ public:
     void update();
 
 private:
+    static auto constexpr sqr = []( auto n ) { return n*n; }; // TODO: refactor?
+
     struct _HitboxEntry { // POD
         IObject            *object; // parent object   (multiple hitboxes can share the same parent)
         Box const          *hitbox; // the hitbox itself
