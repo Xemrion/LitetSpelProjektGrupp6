9037e96c8f1b797a5c2f751448abe9e1ab623314
Xemrion <38206179+Xemrion@users.noreply.github.com>
2019-04-29 13:17:57 +0200

    Added powerUp support for blob and Finished Heavy, started Sticky

--- LitetSpel/LitetSpel/Blob.cpp
+++ LitetSpel/LitetSpel/Blob.cpp
@@ -8,9 +8,11 @@ Blob::Blob( glm::vec3 const &parentPosition ):
     hitbox          ({ glm::vec4(pos,0), glm::vec4(radius,radius,radius,0), glm::vec4(.5, .5, 1.0, .0) }),
     isActive        (false),
     isBeingRecalled (true),
+	isStuck         (false),
     recallSpeed     (200.0f),
     speed           (100.1f),
     radius          (  2.0f),
+	status          (BlobStatus::Blob_None),
     velocity        (   .0f)
 {}
 
@@ -22,10 +24,14 @@ void Blob::absorb() noexcept
 
 void Blob::shoot( glm::vec3 const &direction ) noexcept
 {
-    if ( !isActive && !isBeingRecalled ) {
+    if ( !isActive && !isBeingRecalled && status != BlobStatus::Blob_Heavy) {
         isActive = true;
         velocity = direction * speed;
     }
+	else if (!isActive && !isBeingRecalled && status == BlobStatus::Blob_Heavy) {
+		isActive = true;
+		velocity = direction * (speed/3);
+	}
 }
 
 void Blob::recall() noexcept
@@ -34,7 +40,10 @@ void Blob::recall() noexcept
 }
 
 bool Blob::getIsActive()        const noexcept { return isActive; }
+
 bool Blob::getIsBeingRecalled() const noexcept { return isBeingRecalled; }
+
+bool Blob::getIsStuck() const noexcept { return isStuck; }
 
 // called once per frame from Player::update
 void Blob::update(double dt) noexcept
@@ -44,12 +53,17 @@ void Blob::update(double dt) noexcept
         float speed          = std::min( recallSpeed * float(dt), glm::distance(pos, *parentPosition) );
         glm::vec3 direction  = glm::normalize( *parentPosition - pos );
 		pos                 += speed * direction;
+		isStuck              = false;
 	}
     if (!isActive) {
         pos = *parentPosition + glm::vec3(0.0, 2.0, 0.0);
         setVelocity(glm::vec3(0.0));
         blobSphere.centerRadius = glm::vec4(*parentPosition, 2);
     }
+	if (isStuck == true && status != BlobStatus::Blob_Sticky) 
+	{
+		isStuck = false;
+	}
 	blobSphere.centerRadius = glm::vec4( pos, radius );
 }
 
@@ -91,7 +105,7 @@ void Blob::collide(ColliderType ownType, ColliderType otherType, Box const &othe
         absorb();
     }
 
-	if ( isActive and otherType == ColliderType::platform) {
+	if ( isActive and otherType == ColliderType::platform && status != BlobStatus::Blob_Sticky) {
 		this->velocity      = glm::vec3(0.0);
 		glm::vec3 pushUp    = glm::vec3(0.0, other.center.y + other.halfLengths.y + (-hitbox.center.y + hitbox.halfLengths.y), 0.0);
 		glm::vec3 pushDown  = glm::vec3(0.0, other.center.y - other.halfLengths.y + (-hitbox.center.y - hitbox.halfLengths.y), 0.0);
@@ -100,5 +114,17 @@ void Blob::collide(ColliderType ownType, ColliderType otherType, Box const &othe
 		glm::vec3 minDistY  = glm::length(pushUp)   < glm::length(pushDown)  ? pushUp   : pushDown;
 		glm::vec3 minDistX  = glm::length(pushLeft) < glm::length(pushRight) ? pushLeft : pushRight;
 		pos                += glm::length(minDistY) < glm::length(minDistX)  ? minDistY : minDistX;
+	}
+	else if(isActive and otherType == ColliderType::platform && status == BlobStatus::Blob_Sticky)
+	{
+		this->velocity = glm::vec3(0.0);
+		glm::vec3 pushUp = glm::vec3(0.0, other.center.y + other.halfLengths.y + (-hitbox.center.y + hitbox.halfLengths.y), 0.0);
+		glm::vec3 pushDown = glm::vec3(0.0, other.center.y - other.halfLengths.y + (-hitbox.center.y - hitbox.halfLengths.y), 0.0);
+		glm::vec3 pushRight = glm::vec3(other.center.x + other.halfLengths.x + (-hitbox.center.x + hitbox.halfLengths.x), 0.0, 0.0);
+		glm::vec3 pushLeft = glm::vec3(other.center.x - other.halfLengths.x + (-hitbox.center.x - hitbox.halfLengths.x), 0.0, 0.0);
+		glm::vec3 minDistY = glm::length(pushUp) < glm::length(pushDown) ? pushUp : pushDown;
+		glm::vec3 minDistX = glm::length(pushLeft) < glm::length(pushRight) ? pushLeft : pushRight;
+		pos += glm::length(minDistY) < glm::length(minDistX) ? minDistY : minDistX;
+		isStuck = true;
 	}
 }
