ef1543e3b68503c77ad66a40ba43fcb4bba4eca5
Xemrion <38206179+Xemrion@users.noreply.github.com>
2019-05-16 08:56:29 +0200

    Created Prototype stage 3 and increased maxNumberOfBoxes

--- LitetSpel/LitetSpel/game.cpp
+++ LitetSpel/LitetSpel/game.cpp
@@ -1,7 +1,7 @@
 #include "game.h"
 
 void Game::init() noexcept {
-	editor.initialize("PrototypeOne.png");
+	editor.initialize("PrototypeThree.png");
 	for (int i = 0; i < editor.platforms.size(); i++)
 	{
 		level.staticBoxes.push_back(editor.platforms.at(i).hitbox);
