a43faadaab7e662a2631108f7a3c546db3562009
DavidBjorkberg <David.bjorkberg1@gmail.com>
2019-05-26 14:10:21 +0200

    updated Level

--- LitetSpel/LitetSpel/game.cpp
+++ LitetSpel/LitetSpel/game.cpp
@@ -1,7 +1,7 @@
 #include "game.h"
 
 void Game::init() noexcept {
-	editor.initialize("David.png");
+	editor.initialize("Level.png");
 	// Platforms
 	for (int i = 0; i < editor.platforms.size(); i++)
 	{
